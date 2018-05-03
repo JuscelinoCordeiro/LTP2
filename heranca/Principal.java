16da2bfa5999fb3081f75ce2047c13d95bba3061
hefesto <hefesto@portege>
2018-05-03 20:00:47 -0300

    03-05-18 20:01 tudo debugado

--- heranca/Principal.java
+++ heranca/Principal.java
@@ -3,8 +3,6 @@ package heranca;
 public class Principal {
 
 	public static void main(String[] args) {
-		SeguroVeicular s = new SeguroVeicular("Mascos Paulo", 60000.0, 20/04/2018, 20/04/2019, "plc-9080", 6500.0);
-		System.out.println(s);
 	}
 
 }
